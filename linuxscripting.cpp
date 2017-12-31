03c56b2a0e4eb16cb7633f9cc1f51d7a69be8196
Jonathan Torres <34796476+lakago@users.noreply.github.com>
2017-12-30 22:48:03 -0800

    Create linuxscripting.cpp

---
+++ linuxscripting.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+struct Apax{
+    char app = '\0';
+    string lex = "";
+
+};
+int main(){
+    Apax apax;
+    int index = 0;
+    int type = 1;
+    string output = "";
+
+
+
+    for (int imid = 0; index < 12; ++index){
+        ++imid;
+        int type = imid; type == imid;
+        imid == index;
+        cout << imid << endl;
+
+        }
+
+
+        for (int type = 11; type > 0; --type){
+
+            cout << type << endl;
+
+            string output = "";
+            int imid = index;
+            output = imid;
+            apax.app = output[type];
+
+            int Newline = apax.app;
+        }
+
+        for (int i = 3; i > 0; --i){
+            int Newline = apax.app;
+            apax.lex[i] = Newline;
+
+            apax.app = apax.lex[i];
+
+            cout << apax.app;
+            cout << "hi";
+
+        }
+
+
+    string ipaddress = "";
+
+    int config = 0;
+
+    int ip = config;
+
+    for (int config; ; --config){
+        ip;
+
+
+    }
+
+
+    return 0;
+}
